b3ac2b08181e824326cb23226d4907e9401c3c65
bacon.build <bacondotbuild@gmail.com>
2024-03-31 07:25:12 -0700

    delete www app

--- apps/www/src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-import Main from './main'
-import Title from './title'
-import Button from './button'
-
-export { Main, Title, Button }
--- apps/www/src/components/ui/title.tsx
+++
@@ -1,7 +0,0 @@
-export default function Title({ children }: { children: React.ReactNode }) {
-  return (
-    <h1 className='text-center text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10'>
-      {children}
-    </h1>
-  )
-}
