633b0a681f8d1a69186fed3492884970973a7455
bacon.build <bacondotbuild@gmail.com>
2024-04-01 17:45:50 -0700

    add copy button to test, which uses util from lib workspace

--- apps/test/src/app/page.tsx
+++ apps/test/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Main from '@/app/_components/main'
+import CopyButton from '@/components/copy-button'
 
 export default function Home() {
   return (
     <Main className='flex flex-col p-4'>
       <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
         test
+        <CopyButton />
       </div>
     </Main>
   )
---
+++ apps/test/src/components/copy-button.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { copyToClipboard } from 'lib'
+
+import Button from '@/components/ui/button'
+
+export default function CopyButton() {
+  return (
+    <Button
+      onClick={() => {
+        copyToClipboard('test')
+      }}
+    >
+      copy
+    </Button>
+  )
+}
