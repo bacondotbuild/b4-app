0fc1e62fde2542dc981995f0b4b14b72704d41e9
bacon.build <bacondotbuild@gmail.com>
2024-04-02 16:25:49 -0700

    remove lib dependency from app

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,13 +1,10 @@
-import Main from '@/app/_components/main'
-import { Title } from '@/components/ui'
-import CopyButton from '@/components/copy-button'
+import { Main, Title } from '@/components/ui'
 
 export default function Home() {
   return (
     <Main className='flex flex-col p-4'>
       <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
-        <Title>home</Title>
-        <CopyButton />
+        <Title>b4</Title>
       </div>
     </Main>
   )
--- apps/app/src/components/ui/index.ts
+++ apps/app/src/components/ui/index.ts
@@ -1,5 +1,4 @@
 import Main from './main'
 import Title from './title'
-import Button from './button'
 
-export { Main, Title, Button }
+export { Main, Title }
