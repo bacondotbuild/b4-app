3f6c9dc17177c912f80a1d37ae4349c2de043d0c
bacon.build <bacondotbuild@gmail.com>
2024-04-02 19:28:59 -0700

    add fetcher example to lib

--- apps/lib/src/app/[util]/page.tsx
+++ apps/lib/src/app/[util]/page.tsx
@@ -1,10 +1,17 @@
+import Link from 'next/link'
+
+import { Title } from 'ui'
+
 import { Main } from '@/components/ui'
 import Utils from './utils'
 
 export default function UtilPage({ params }: { params: { util: string } }) {
   return (
     <Main className='flex flex-col p-4'>
-      <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
+      <div className='flex flex-grow flex-col space-y-4'>
+        <Link href='/' className='hover:text-cb-pink/75'>
+          <Title>lib</Title>
+        </Link>
         <Utils util={params.util} />
       </div>
     </Main>
--- apps/lib/src/app/[util]/utils.tsx
+++ apps/lib/src/app/[util]/utils.tsx
@@ -1,9 +1,9 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
-import { Title } from 'ui'
-import { copyToClipboard } from 'lib'
+import { copyToClipboard, fetcher } from 'lib'
 
 import { Button } from '@/components/ui'
 import utils from '@/lib/lib'
@@ -21,8 +21,63 @@ function CopyToClipboard() {
   )
 }
 
+type Repo = {
+  id: number
+  name: string
+  html_url: string
+  homepage: string
+}
+
 function Fetcher() {
-  return <div>fetcher</div>
+  const [repos, setRepos] = useState([] as Repo[])
+
+  useEffect(() => {
+    async function fetchRepos() {
+      const res: Repo[] = await fetcher<Repo[]>(
+        'https://api.github.com/users/bacondotbuild/repos?sort=updated'
+      )
+      const newRepos = res.map(repo => ({
+        id: repo.id,
+        name: repo.name,
+        html_url: repo.html_url,
+        homepage: repo.homepage,
+      }))
+      setRepos(newRepos)
+    }
+    void fetchRepos()
+  }, [])
+
+  if (!repos || repos.length === 0) {
+    return <p>fetching...</p>
+  }
+  return (
+    <ul className='divide-y divide-cb-dusty-blue'>
+      <li className='flex items-center py-4 first:pt-0'>
+        <span className='grow text-lg'>repo</span>
+        <span className='text-lg'>link</span>
+      </li>
+      {repos.map(repo => (
+        <li key={repo.id} className='flex items-center py-4 first:pt-0'>
+          <a
+            className='grow text-xl text-cb-pink hover:text-cb-pink/75'
+            href={repo.html_url}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {repo.name}
+          </a>
+          <a
+            className='text-cb-pink hover:text-cb-pink/75'
+            href={repo.homepage}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {repo.homepage}
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 function UseForm() {
@@ -53,9 +108,7 @@ export default function Utils({ util }: { util: string }) {
   const Component = utilsToComponents[currentUtil]
   return (
     <>
-      <Title>{util}</Title>
-      {Component && <Component />}
-      <ul className='space-y-4'>
+      <ul className='flex space-x-4'>
         {utils.map(u => (
           <li key={u}>
             {u === util ? (
@@ -70,12 +123,8 @@ export default function Utils({ util }: { util: string }) {
             )}
           </li>
         ))}
-        <li>
-          <Link href='/' className='text-cb-pink hover:text-cb-pink/75'>
-            home
-          </Link>
-        </li>
       </ul>
+      {Component && <Component />}
     </>
   )
 }
