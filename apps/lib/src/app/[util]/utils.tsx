877277cb1c7a4c8e1244b5465da21ffd531ecef6
bacon.build <bacondotbuild@gmail.com>
2024-04-05 18:49:56 -0700

    add useSearch example to lib

--- apps/lib/src/app/[util]/utils.tsx
+++ apps/lib/src/app/[util]/utils.tsx
@@ -3,7 +3,13 @@
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
-import { copyToClipboard, fetcher, useForm, useLocalStorage } from 'lib'
+import {
+  copyToClipboard,
+  fetcher,
+  useForm,
+  useLocalStorage,
+  useSearch,
+} from 'lib'
 
 import { Button } from '@/components/ui'
 import utils from '@/lib/lib'
@@ -202,7 +208,44 @@ function UseLocalStorage() {
 }
 
 function UseSearch() {
-  return <div>useSearch</div>
+  const list = [
+    { id: '1', title: 'title 1', body: 'body 1' },
+    { id: '2', title: 'title 2', body: 'body 2' },
+  ]
+  const { search, setSearch, results, searchRef } = useSearch({
+    initialSearch: '',
+    list,
+    options: {
+      keys: ['title', 'body'],
+    },
+  })
+  return (
+    <div className='space-y-4'>
+      <input
+        ref={searchRef}
+        type='text'
+        className='w-full bg-cb-blue'
+        placeholder='search'
+        value={search}
+        onChange={e => {
+          const { value } = e.target
+          setSearch(value)
+        }}
+      />
+      {results?.length && results?.length > 0 ? (
+        <ul className='divide-y divide-cb-dusty-blue'>
+          {results.map(result => (
+            <li key={result.id} className='py-4 first:pt-0'>
+              <p>{result.title}</p>
+              <p>{result.body}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>no results</p>
+      )}
+    </div>
+  )
 }
 
 const utilsToComponents: Record<string, React.FC> = {
