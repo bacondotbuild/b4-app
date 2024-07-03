f6cb0f12a46103ee7c5e6cc690f0322aa6e0912e
bacon.build <bacondotbuild@gmail.com>
2024-07-03 13:00:39 -0700

    add link to create b4 app in readme

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <Main className='flex flex-col p-4'>
       <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
-        <Title>b4</Title>
+        <Title>
+          <a
+            href='https://github.com/bacondotbuild/b4-app/blob/develop/apps/app/README.md#create-b4-app'
+            target='_blank'
+            className='hover:text-cb-pink'
+          >
+            b4
+          </a>
+        </Title>
       </div>
     </Main>
   )
